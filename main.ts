e2080176dd241dd386aa8a60c9487a2cdd14ccb3
KeldJensen <45073844+KeldJensen@users.noreply.github.com>
2018-11-20 14:59:41 +0100

    First commit

--- main.ts
+++ main.ts
@@ -1,7 +1,15 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+input.onButtonPressed(Button.A, function () {
+    basic.showString("A")
+    while (!(input.buttonIsPressed(Button.B))) {
+        radio.sendNumber(input.acceleration(Dimension.X))
+    }
+    basic.showString("B")
+})
+radio.onReceivedNumber(function (receivedNumber) {
+    serial.writeNumbers([radio.receivedSerial(), receivedNumber])
+})
+serial.redirectToUSB()
+radio.setGroup(1)
+radio.setTransmitSerialNumber(true)
+input.setAccelerometerRange(AcceleratorRange.OneG)
+ 
